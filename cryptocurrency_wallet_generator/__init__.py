7045bfcf401de94d21fede66d607cc2c2c836ced
Pastor Ombura <36679951+PastorOmbura@users.noreply.github.com>
2018-04-20 20:35:27 +0300

    Update __init__.py
    Add SalemCash to Python Cryptocurrency Wallet Generator

--- cryptocurrency_wallet_generator/__init__.py
+++ cryptocurrency_wallet_generator/__init__.py
@@ -3,6 +3,7 @@ from .currencies.bitcoincash import BitcoinCash
 from .currencies.dash import Dash
 from .currencies.ethereum import Ethereum
 from .currencies.litecoin import Litecoin
+from .currrencis.salemcash import SalemCash
 
 
 currencies = {
@@ -11,9 +12,10 @@ currencies = {
     "Dash": Dash,
     "Ethereum": Ethereum,
     "Litecoin": Litecoin,
+    "Salem Cash": SalemCash
 }
 
 
-def generate_wallet(currency_name):
-    currency = currencies[currency_name]()
+def generate_wallet(Salem_Cash):
+    currency = currencies[salem_cash]()
     return currency.generate_wallet()
